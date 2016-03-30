a38543aebe61ac7fb46cfbc284e86ce982b00a43
Matthew Brookes <mbrookes1304@gmail.com>
2016-03-30 16:48:32 +0100

    Implement Repeating Key XOR

--- Bitwise.py
+++ Bitwise.py
@@ -8,3 +8,21 @@ def fixedXOR(hex1, hex2):
         hex_sum_as_int *= (16*16)
         hex_sum_as_int += x^y
     return "{0:0{1}x}".format(hex_sum_as_int, len(hex1))
+
+def repeat_key_XOR(plaintext, key):
+    ''' Returns encrypted form of plaintext using the key for repeat XOR '''
+    hex_rep_plaintext = base64.b16encode(plaintext.encode())
+    cipher_text = ''
+    key_index = 0
+    bytes_in_hex_rep_plaintext = [
+        hex_rep_plaintext[i: i+2] for i in range(0, len(hex_rep_plaintext), 2)
+    ]
+    for byte in bytes_in_hex_rep_plaintext:
+        key_char = key[key_index]
+        hex_rep_key_char = base64.b16encode(key_char.encode())
+        xor = fixedXOR(hex_rep_key_char, byte)
+        cipher_text += xor
+        key_index += 1
+        if key_index == len(key):
+            key_index = 0
+    return cipher_text
